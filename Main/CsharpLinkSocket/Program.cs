b88972821f323b87c95ebcc947eba8ef4ed4b4fd
linyun <2411766368@qq.com>
2019-11-11 14:40:27 +0800

    Main程序添加Socket通信模块

---
+++ Main/CsharpLinkSocket/Program.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Security.Cryptography;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace CsharpLinkSocket
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CsharpSocket csharpSocket = new CsharpSocket("127.0.0.1",4300);
+            Console.WriteLine("按任意键退出");
+            Console.ReadKey();
+        }
+    }
+    class CsharpSocket
+    {
+        private Socket socket;
+        private Socket clientSocket;
+        private byte[] buffer = new byte[1024];
+        private int length;
+        private Boolean run=true;
+        public CsharpSocket(string ip,int port)
+        {
+            IPEndPoint point = new IPEndPoint(IPAddress.Parse(ip), port);
+            socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            try
+            {
+                socket.Bind(point);
+                socket.Listen(10);
+                Console.WriteLine("等待客户端连接....");
+                clientSocket = socket.Accept();
+                clientSocket.Blocking = true;
+                Console.WriteLine("接收到了客户端：ip" + clientSocket.RemoteEndPoint.ToString() + "的连接");
+                length = clientSocket.Receive(buffer);
+                clientSocket.Send(PackHandShakeData(GetSecKeyAccetp(buffer, length)));
+                Console.WriteLine("已经发送握手协议了....");
+                length = clientSocket.Receive(buffer);
+                string xm = AnalyticData(buffer, length);
+                clientSocket.Send(PackData("连接时间：" + DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss")));
+                Task readdata = new Task(() =>
+                {
+                    ReadData();
+                });
+                Task senddata = new Task(() =>
+                {
+                    while (run)
+                    {
+                        String str = Console.ReadLine();
+                        SendData(str);
+                    }
+                });
+                readdata.Start();
+                senddata.Start();
+                readdata.Wait();
+                senddata.Wait();
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+        public void ReadData()
+        {
+            try
+            {
+                while (run)
+                {
+                    Console.WriteLine("等待客户端数据....");
+                    length = clientSocket.Receive(buffer);
+                    string clientMsg = AnalyticData(buffer, length);
+                    if (clientMsg.Equals("exit")) run = false;
+                    Console.WriteLine("接受到客户端数据：" + clientMsg);
+                    string sendMsg = "谢谢你的数据：" + clientMsg;
+                    SendData(sendMsg);
+                }
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+        public void SendData(String str)
+        {
+            clientSocket.Send(PackData(str));
+        }
+        private static string GetSecKeyAccetp(byte[] handShakeBytes, int bytesLength)
+        {
+            string handShakeText = Encoding.UTF8.GetString(handShakeBytes, 0, bytesLength);
+            string key = string.Empty;
+            Regex r = new Regex(@"Sec\-WebSocket\-Key:(.*?)\r\n");
+            Match m = r.Match(handShakeText);
+            if (m.Groups.Count != 0)
+            {
+                key = Regex.Replace(m.Value, @"Sec\-WebSocket\-Key:(.*?)\r\n", "$1").Trim();
+            }
+            byte[] encryptionString = SHA1.Create().ComputeHash(Encoding.ASCII.GetBytes(key + "258EAFA5-E914-47DA-95CA-C5AB0DC85B11"));
+            return Convert.ToBase64String(encryptionString);
+        }
+        private static byte[] PackHandShakeData(string secKeyAccept)
+        {
+            var responseBuilder = new StringBuilder();
+            responseBuilder.Append("HTTP/1.1 101 Switching Protocols" + Environment.NewLine);
+            responseBuilder.Append("Upgrade: websocket" + Environment.NewLine);
+            responseBuilder.Append("Connection: Upgrade" + Environment.NewLine);
+            responseBuilder.Append("Sec-WebSocket-Accept: " + secKeyAccept + Environment.NewLine + Environment.NewLine);
+            //如果把上一行换成下面两行，才是thewebsocketprotocol-17协议，但居然握手不成功，目前仍没弄明白！
+            //responseBuilder.Append("Sec-WebSocket-Accept: " + secKeyAccept + Environment.NewLine);
+            //responseBuilder.Append("Sec-WebSocket-Protocol: chat" + Environment.NewLine);
+
+            return Encoding.UTF8.GetBytes(responseBuilder.ToString());
+        }
+        private static string AnalyticData(byte[] recBytes, int recByteLength)
+        {
+            if (recByteLength < 2) { return string.Empty; }
+
+            bool fin = (recBytes[0] & 0x80) == 0x80; // 1bit，1表示最后一帧  
+            if (!fin)
+            {
+                return string.Empty;// 超过一帧暂不处理 
+            }
+
+            bool mask_flag = (recBytes[1] & 0x80) == 0x80; // 是否包含掩码  
+            if (!mask_flag)
+            {
+                return string.Empty;// 不包含掩码的暂不处理
+            }
+
+            int payload_len = recBytes[1] & 0x7F; // 数据长度  
+
+            byte[] masks = new byte[4];
+            byte[] payload_data;
+
+            if (payload_len == 126)
+            {
+                Array.Copy(recBytes, 4, masks, 0, 4);
+                payload_len = (UInt16)(recBytes[2] << 8 | recBytes[3]);
+                payload_data = new byte[payload_len];
+                Array.Copy(recBytes, 8, payload_data, 0, payload_len);
+
+            }
+            else if (payload_len == 127)
+            {
+                Array.Copy(recBytes, 10, masks, 0, 4);
+                byte[] uInt64Bytes = new byte[8];
+                for (int i = 0; i < 8; i++)
+                {
+                    uInt64Bytes[i] = recBytes[9 - i];
+                }
+                UInt64 len = BitConverter.ToUInt64(uInt64Bytes, 0);
+
+                payload_data = new byte[len];
+                for (UInt64 i = 0; i < len; i++)
+                {
+                    payload_data[i] = recBytes[i + 14];
+                }
+            }
+            else
+            {
+                Array.Copy(recBytes, 2, masks, 0, 4);
+                payload_data = new byte[payload_len];
+                Array.Copy(recBytes, 6, payload_data, 0, payload_len);
+
+            }
+
+            for (var i = 0; i < payload_len; i++)
+            {
+                payload_data[i] = (byte)(payload_data[i] ^ masks[i % 4]);
+            }
+
+            return Encoding.UTF8.GetString(payload_data);
+        }
+        private static byte[] PackData(string message)
+        {
+            byte[] contentBytes = null;
+            byte[] temp = Encoding.UTF8.GetBytes(message);
+
+            if (temp.Length < 126)
+            {
+                contentBytes = new byte[temp.Length + 2];
+                contentBytes[0] = 0x81;
+                contentBytes[1] = (byte)temp.Length;
+                Array.Copy(temp, 0, contentBytes, 2, temp.Length);
+            }
+            else if (temp.Length < 0xFFFF)
+            {
+                contentBytes = new byte[temp.Length + 4];
+                contentBytes[0] = 0x81;
+                contentBytes[1] = 126;
+                contentBytes[2] = (byte)(temp.Length & 0xFF);
+                contentBytes[3] = (byte)(temp.Length >> 8 & 0xFF);
+                Array.Copy(temp, 0, contentBytes, 4, temp.Length);
+            }
+            else
+            {
+                // 暂不处理超长内容  
+            }
+
+            return contentBytes;
+        }
+    }
+}
