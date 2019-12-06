5c3839cb94c5cf9d873e95e4b30bb7edd87c7ea9
linyun <2411766368@qq.com>
2019-12-06 11:28:33 +0800

    https://gitee.com/Lin-Yun/game_simulation_of_yunding/issues/I16422

--- electronApp/angular/src/app/components/timer/timer.component.ts
+++ electronApp/angular/src/app/components/timer/timer.component.ts
@@ -7,6 +7,8 @@ var time
   styleUrls: ['./timer.component.scss']
 })
 export class TimerComponent implements OnInit {
+  protected textColor:string = 'white'
+  private maxTime:number
   imgsrc = 'whiteTime'
   rotate: number = 0
   public thisTime
@@ -15,22 +17,40 @@ export class TimerComponent implements OnInit {
 
   ngOnInit() {
     time = new Date()
-    this.start()
-  }
-  start() {
     this.thisTime = time.getTime()
-    this.run()
+    this.TimeOut()
   }
-  run() {
+  TimeReady(){
+    this.maxTime = 3
+  }
+  TimeOut(){
+    this.textColor = 'red'
+    this.imgsrc = 'redTime'
+    this.maxTime = 10
+    this.run(this.end)
+  }
+  FightStart(){
+    this.textColor = 'white'
+    time = new Date()
+    this.maxTime = 30
+    // this.run()
+  }
+  private run(fun:any){
     let time = new Date()
     let t = time.getTime() - this.thisTime
-    let num = 30 - parseInt((t / 1000).toString())
+    let num = this.maxTime - parseInt((t / 1000).toString())
     this.rotate = 15 * parseInt((t * 24 / 1000).toString())
     this.setTime = num
     if(num > 0){
       setTimeout(() => {
-        this.run()
+        this.run(fun)
       }, 40);
     }
+    else if(num == 0){
+      fun()
+    }
+  }
+  end(){
+    alert('end')
   }
 }
