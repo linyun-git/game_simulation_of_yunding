33e1dcd460b1f1abd8b76e14f0d377adb73218d2
Wingel <1092164597@qq.com>
2019-12-06 22:21:31 +0800

    简化了xml解析的代码，优化了xml文档的格式

--- lib/ConsoleApp1/ConsoleApp1/Program.cs
+++ lib/ConsoleApp1/ConsoleApp1/Program.cs
@@ -8,32 +8,49 @@ namespace ConsoleApp1
 
         static void Main(string[] args)
         {
+            String data = null;
+            // data = XMLjiexi.GetDetail("奥恩", "LV2", "hp");
 
-            XMLjiexi.Get("s", "s", "s");
-            
+            // data = XMLjiexi.GetRace("奥恩");
+
+            // data = XMLjiexi.GetCareer("奥恩");
+
+            Console.WriteLine(data);
+            Console.ReadLine();
         }
     }
     class XMLjiexi
     {
-        public static string Get(string name, string lv ,string detail)
+        //  获取英雄不同等级的技能（skill），护甲(adr)，生命值(hp)，攻击力(ad)  输入参数为（英雄的名字，等级，所需内容）
+        public static string GetDetail(string name, string lv ,string detail)
         {
-            string text = null;
-            XmlDocument doc = new XmlDocument();
-            doc.Load(@"C:\Users\fengruoling\Desktop\yunding\game_simulation_of_yunding\lib\Hero\hero_attribute.xml");
-
-            XmlElement root = null;
-            root = doc.DocumentElement;
-
-            XmlNodeList listNodes = null;
-            listNodes = root.SelectNodes("/HeroList/hero/name");
-
-             foreach (XmlNode node in listNodes)
-            {
-                text += node.InnerText + "\n";
-            }
-            Console.WriteLine(text);
-            Console.ReadLine();
-            return text;
+            string informationNeeded = "/HeroList/hero[@name='" + name + "']/detail/" + lv + "/" + detail;
+            XmlDocument dom = new XmlDocument();
+            dom.Load(@"C:\Users\fengruoling\Desktop\yunding\game_simulation_of_yunding\lib\Hero\hero_attribute.xml");
+            XmlElement root = dom.DocumentElement;
+            XmlElement node = (XmlElement)root.SelectSingleNode(informationNeeded);
+            return node.InnerText;
         }
+        // 获取英雄的种族  输入参数为（英雄的名字）
+        public static string GetRace(string name)
+        {
+            string informationNeeded = "/HeroList/hero[@name='" + name + "']/race";
+            XmlDocument dom = new XmlDocument();
+            dom.Load(@"C:\Users\fengruoling\Desktop\yunding\game_simulation_of_yunding\lib\Hero\hero_attribute.xml");
+            XmlElement root = dom.DocumentElement;
+            XmlElement node = (XmlElement)root.SelectSingleNode(informationNeeded);
+            return node.InnerText;
+        }
+        // 获取英雄的职业  输入参数为（英雄的名字）
+        public static string GetCareer(string name)
+        {
+            string informationNeeded = "/HeroList/hero[@name='" + name + "']/career";
+            XmlDocument dom = new XmlDocument();
+            dom.Load(@"C:\Users\fengruoling\Desktop\yunding\game_simulation_of_yunding\lib\Hero\hero_attribute.xml");
+            XmlElement root = dom.DocumentElement;
+            XmlElement node = (XmlElement)root.SelectSingleNode(informationNeeded);
+            return node.InnerText;
+        }
+
     }
 }
