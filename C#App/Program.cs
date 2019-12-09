d58be84756aeda0329e90f4eaa0d0c9989a2dd9a
Dale <1165566528@qq.com>
2019-12-09 23:02:52 +0800

    程序中加入xml数据

--- C#App/Program.cs
+++ C#App/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Threading;
+using System.Xml;
 
 namespace ConsoleApp1
 {
@@ -43,6 +44,7 @@ namespace ConsoleApp1
 
         static void Main()
         {
+            Hero[,] qp = new Hero[8, 7];
             Hero Weien = new Hero() { Name = "薇恩", ATN = 40, DEF = 25, HP = 500, MAXHP = 500, ASD = 0.25, Dead = 0 },
                 Maokai = new Hero() { HP = 650, Name = "茂凯", ATN = 55, DEF = 35, MAXHP = 650, ASD = 0.5, Dead = 0 };
             Thread t1 = new Thread(new ParameterizedThreadStart(Weien.Attack));
@@ -60,5 +62,39 @@ namespace ConsoleApp1
                 Console.WriteLine("{0}胜利！", Maokai.Name);
             }
         }
+    }
+    class XMLjiexi
+    {
+        //  获取英雄不同等级的技能（skill），护甲(adr)，生命值(hp)，攻击力(ad)  输入参数为（英雄的名字，等级，所需内容）
+        public static string GetDetail(string name, string lv, string detail)
+        {
+            string informationNeeded = "/HeroList/hero[@name='" + name + "']/detail/" + lv + "/" + detail;
+            XmlDocument dom = new XmlDocument();
+            dom.Load(@"..\..\..\..\Hero\hero_attribute.xml");
+            XmlElement root = dom.DocumentElement;
+            XmlElement node = (XmlElement)root.SelectSingleNode(informationNeeded);
+            return node.InnerText;
+        }
+        // 获取英雄的种族  输入参数为（英雄的名字）
+        public static string GetRace(string name)
+        {
+            string informationNeeded = "/HeroList/hero[@name='" + name + "']/race";
+            XmlDocument dom = new XmlDocument();
+            dom.Load(@"..\..\..\..\Hero\hero_attribute.xml");
+            XmlElement root = dom.DocumentElement;
+            XmlElement node = (XmlElement)root.SelectSingleNode(informationNeeded);
+            return node.InnerText;
+        }
+        // 获取英雄的职业  输入参数为（英雄的名字）
+        public static string GetCareer(string name)
+        {
+            string informationNeeded = "/HeroList/hero[@name='" + name + "']/career";
+            XmlDocument dom = new XmlDocument();
+            dom.Load(@"..\..\..\..\Hero\hero_attribute.xml");
+            XmlElement root = dom.DocumentElement;
+            XmlElement node = (XmlElement)root.SelectSingleNode(informationNeeded);
+            return node.InnerText;
+        }
+
     }
 }
