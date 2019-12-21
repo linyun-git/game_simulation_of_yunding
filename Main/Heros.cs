4ee43ee00951e2e7255b9bf6ad6ccbe988923e4e
Dale <1165566528@qq.com>
2019-12-22 00:12:20 +0800

    https://gitee.com/Lin-Yun/game_simulation_of_yunding/issues/I1744V

--- Main/Heros.cs
+++ Main/Heros.cs
@@ -7,8 +7,23 @@ namespace Main
     ///<summary>茂凯</summary>
     class MAOKAI : Hero
     {
-        public MAOKAI()
+        public MAOKAI(int level,int id)
         {
+            heroName = "茂凯";
+            heroLevel = level;
+            if (id <= 27)
+            {
+                color = Blue;
+            }
+            else
+            {
+                color = Red;
+            }
+            init();
+        }
+        public new void skill()
+        {
+
         }
     }
 }
